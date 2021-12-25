6b24c60f88c8dd2a908f880b37aa4b077bd41b9a
Aimee2012 <amylinsy@gmail.com>
2021-12-25 15:35:13 -0400

    Modificados archivos para manejar dos eventos más

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,11 +15,22 @@ export class AppComponent {
     age: 18,
     avatar: 'https://www.w3schools.com/howto/img_avatar.png'
   }
+
   toggleButton() {
     this.btnDisabled = !this.btnDisabled;
   }
 
   increaseAge() {
     this.person.age += 1;
+  }
+
+  onScroll(event: Event) {
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  changeName(event: Event) {
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
   }
 }
